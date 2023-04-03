2ee670049957b2ab0ff006c5e2cd3a81463d95c7
Roman <roman.itdtin@gmail.com>
2023-04-03 11:15:22 +0530

    fix

--- app/api_requests.py
+++ app/api_requests.py
@@ -30,6 +30,7 @@ def get(url, headers=None, params=None):
             time.sleep(10)
 
 
+
 def get_swap_data():
     url = "https://swap.metaswap.codefi.network/networks/42161/trades?sourceAmount=10000000000000000&sourceToken=0x0000000000000000000000000000000000000000&destinationToken=0x11cdb42b0eb46d95f990bedd4695a6e3fa034978&slippage=3&walletAddress=0xc17e5dec5a1f32a947d4456906fe53079215f76b&timeout=10000&enableDirectWrapping=true&includeRoute=true"
 
